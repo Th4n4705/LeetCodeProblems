5a6d3be030856b38d7aad35cdd48f3aa35c7fff0
Th4n4705 <taha.wakrim.pro@gmail.com>
2022-09-10 18:42:28 +0100

    feat(Solve The 292 problem):

---
+++ 292 Nim Game.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CanWinNim(int n) {
+        
+        if (n <= 3)
+            return true;
+        
+        if (n % 4 == 0)
+            return false;
+        
+        return true;
+    }
+}
