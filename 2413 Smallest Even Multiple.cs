1e212fbf37a12e393017f6d29d658697d1c43434
Th4n4705 <taha.wakrim.pro@gmail.com>
2022-09-20 11:45:31 +0100

    style(same logic on the 2413 problem): remove the else statement

--- 2413 Smallest Even Multiple.cs
+++ 2413 Smallest Even Multiple.cs
@@ -1,9 +1,8 @@
 public class Solution {
     public int SmallestEvenMultiple(int n) {
-        
-        if (n % 2 == 0)
-            return n;
-        else
+
+        if (n % 2 != 0)
             return n * 2;
+        return n;
     }
 }
