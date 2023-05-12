fb6db40b2beae9044fdb3e4a50db7c728a04b386
Taha WAKRIM <taha.wakrim.pro@gmail.com>
2023-05-12 20:50:41 +0000

    feat(solve the 1837 problem)

---
+++ 1837 Sum of Digits in Base K.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int SumBase(int n, int k) {
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += n % k;
+            n /=k;
+        }
+        return sum;
+    }
+}
