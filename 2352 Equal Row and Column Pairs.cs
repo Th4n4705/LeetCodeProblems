38d5c1659d2ded48de468b1aeca347117c72be3a
Taha WAKRIM <taha.wakrim.pro@gmail.com>
2023-06-14 10:16:56 +0100

    fix(solve the 2352 problem)

--- 2352 Equal Row and Column Pairs.cs
+++ 2352 Equal Row and Column Pairs.cs
@@ -3,14 +3,11 @@ public class Solution {
         int ans = 0;
         for (int i = 0; i < g.Length; i++)
             for (int j = 0; j < g.Length; j++)
-            {
-                int check = 0;
                 for (int k = 0; k < g.Length; k++)
-                    if (g[i][k] == g[k][j])
-                        check++;
-                if (check == g.Length)
-                    ans++;
-            }
+                    if (g[i][k] != g[k][j])
+                        break;
+                    else if (k == g.Length - 1)
+                        ans++;
         return ans;
     }
 }
