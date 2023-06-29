fb6406f0a80f1829ec98cf42a9d6fedc346aec02
Taha WAKRIM <taha.wakrim.pro@gmail.com>
2023-06-29 13:52:52 +0100

    feat(solve the 2656 problem)

---
+++ 2656 Maximum Sum With Exactly K Elements.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaximizeSum(int[] nums, int k) {
+        int max = nums.Max();
+        int counter = max;
+        for (int i = 1; i < k; i++)
+        {
+            max += 1;
+            counter += max;
+        }
+        return counter;
+    }
+}
