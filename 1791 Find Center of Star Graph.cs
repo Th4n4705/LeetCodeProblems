ea6dc952ed4f64988e1d664d8b089bd9eca1aa22
Taha WAKRIM <taha.wakrim.pro@gmail.com>
2023-12-08 19:07:48 +0100

    feat(solve the 1791 problem)

---
+++ 1791 Find Center of Star Graph.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int FindCenter(int[][] edges){
+        if(edges[0][0] == edges [1][0] || edges[0][0] == edges [1][1])
+            return edges [0][0];
+        return edges[0][1];
+    }
+}
