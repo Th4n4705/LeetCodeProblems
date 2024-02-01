189ebd2085b3eb7e9a51a9749bee04d7cdcdc04c
Th4n4705 <taha.wakrim.pro@gmail.com>
2024-02-01 15:03:07 +0100

    feat(Solve the 1290 problem)

---
+++ 1290 Convert Binary Number in a Linked List to Integer.cs
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public int GetDecimalValue(ListNode head) {
+        StringBuilder sb = new ();
+        while (head != null)
+        {
+            sb.Append(head.val);
+            head = head.next;
+        }
+        return Convert.ToInt32(sb.ToString(), 2);
+    }
+}
